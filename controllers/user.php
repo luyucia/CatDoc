05b41abae1c7a5ce6010cc6c7f93bd3fa5f8f711
limingze <limingze@163.com>
2016-01-15 16:21:40 +0800

    add user

---
+++ controllers/user.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @name userController
+ * @author Luyu
+ * @desc 通用用户模块
+ */
+class userController extends BaseController {
+
+    // 注册
+    public function register()
+    {
+        // 判断用户是否已经注册
+        // 校验验证码
+        echo 'ok';
+    }
+    // 登陆
+    public function login()
+    {
+        echo 'ok';
+
+    }
+    // 登出
+    public function logout()
+    {
+
+    }
+    // 发送验证码
+    public function sendValidateCode()
+    {
+
+    }
+    // 校验验证码
+    public function validateCode()
+    {
+
+    }
+    // 修改信息
+    public function edit()
+    {
+
+    }
+    // 重置密码
+    public function resetPassword()
+    {
+
+    }
+
+}
+
+?>
